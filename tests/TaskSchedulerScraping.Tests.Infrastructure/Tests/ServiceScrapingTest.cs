c1bb9ef3a1237bf4b6b9e3204288fe99b3107eea
GuilhermeBley <guilhermecbley@gmail.com>
2022-09-02 21:12:11 -0300

    add test scraping model

--- tests/TaskSchedulerScraping.Tests.Infrastructure/Tests/ServiceScrapingTest.cs
+++ tests/TaskSchedulerScraping.Tests.Infrastructure/Tests/ServiceScrapingTest.cs
@@ -4,15 +4,38 @@ namespace TaskSchedulerScraping.Tests.Infrastructure.Tests;
 
 public class ServiceScrapingTest : InfrastructureTestBase
 {
+    private readonly IScrapingService _scrapingService;
+
+    public ServiceScrapingTest()
+    {
+        _scrapingService = _serviceProvider.GetService<IScrapingService>() ?? 
+            throw new ArgumentNullException(nameof(IScrapingService));
+    }
+
     [Fact]
     public async Task Model_Get_ViewAllModels()
     {
-        var scrapingService = 
-            _serviceProvider.GetService<IScrapingService>() ?? 
-            throw new ArgumentNullException(nameof(IScrapingService));
-
-        var allModels = await scrapingService.GetAllScrapingModelAsync();
+        var allModels = await _scrapingService.GetAllScrapingModelAsync();
 
         Assert.NotNull(allModels);
+    }
+
+    [Fact]
+    public async Task Model_AddAndDelete_AddAndDeleteModel()
+    {
+        var entity =
+            new Application.Dto.Scraping.ScrapingModelDto()
+            {
+                Description = "Test",
+                Name = "Test"
+            };
+
+        var modelCreate = await _scrapingService.AddScrapingModelAsync(entity);
+
+        Assert.NotNull(modelCreate);
+
+        var modelDeleted = await _scrapingService.DeleteScrapingModelByIdAsync(modelCreate.Id);
+
+        Assert.NotNull(modelDeleted);
     }
 }
