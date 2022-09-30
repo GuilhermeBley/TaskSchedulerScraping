2c7c415174ead39eeb6b19bc86a1c07c066dd120
GuilhermeBley <guilhermecbley@gmail.com>
2022-09-30 09:58:16 -0300

    update ScraperObjSharedAttribute

--- src/TaskSchedulerScraping.Scraper.DependencyInjection/Attributes/ScraperObjSharedAttribute.cs
+++ src/TaskSchedulerScraping.Scraper.DependencyInjection/Attributes/ScraperObjSharedAttribute.cs
@@ -3,8 +3,4 @@ namespace TaskSchedulerScraping.Scraper.DependencyInjection.Attributes;
 [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
 public class ScraperObjSharedAttribute : Attribute
 {
-    public ScraperObjSharedAttribute()
-    {
-        throw new ArgumentException();
-    }
 }
